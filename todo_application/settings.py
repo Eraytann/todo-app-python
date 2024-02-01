edd632326560a128d804df189af9a800917672c1
Eray Tan <eraytan@yahoo.com>
2024-02-01 14:28:22 +0300

    allowed host updated

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wb&xfh-c-2p7hcy5n#ky*#jm2@)st%=!r3n!at0orgi(#6fs7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'todo-app-with-python.onrender.com'
+]
 
 
 # Application definition
